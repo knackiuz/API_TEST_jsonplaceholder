5161d14c00b82430785546569d3889092b4f2741
knackiuz <knackiuz@gmail.com>
2026-01-20 12:47:34 +0200

    updated comments - simple and readable added Logged changed System.out.println -> info (Logger)

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -3,6 +3,8 @@ package tests;
 import models.PostModel;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,11 +15,9 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-//API Test class demonstrating best practices for Rest Assured:
-//- Uses the given/when/then structure.
-//- Uses POJO (PostModel) for request/response data handling.
 public class ApiTest {
     //private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final Logger log = LoggerFactory.getLogger(ApiTest.class);
     private static String BASE_URL;
     private static final String POSTS_ENDPOINT = "/posts";
 
@@ -33,36 +33,35 @@ public class ApiTest {
 
             //Set BASE_URL using the value from properties file
             BASE_URL = properties.getProperty("baseUrl");
-            System.out.println("API Tests will run against: " + BASE_URL);
+            //System.out.println("API Tests will run against: " + BASE_URL);
+            log.info("API Tests will run against: {}", BASE_URL);
         } catch (IOException e) {
-            System.err.println("Could not load properties file" + environmentFileName);
+            //System.err.println("Could not load properties file" + environmentFileName);
+            log.info("Could not load properties file: {}", environmentFileName);
         }
 
     }
 
-    //Test case to verify successful creation of a new post resource using POJO.
-    //This method shows both Hamcrest validation and Java/JUnit assertion methods.
+    //Test case to verify successful creation of a new post resource using POJO
     @Test
     void shouldCreateNewPostUsingPojoSuccessfully() {
-        //Arrange (Data Preparation): Create the POJO object (PostModel) which will be sent as the request body.
+        //Create the POJO object (PostModel) which will be sent as the request body
         PostModel requestPayLoad = new PostModel(
                 "Test Automation Assigment",
                 "Testing API resource creation with Rest Assured and POJO.",
                 101
         );
-        //Act & Assert (Execution and Validation)
-        PostModel responseBody = given()                            // GIVEN (Configuration Step): Set the base parameters for the request
+        //Execution and Validation by Rest Assured
+        PostModel responseBody = given()                            //Set the base parameters for the request
                 .baseUri(BASE_URL)
                 .contentType("application/json")                 // Specify that the request body is in JSON format
                 .body(requestPayLoad)                               // Rest Assured automatically serializes the requestPayload object to JSON
-                .when()                                             // WHEN (Action Step): Execute the POST request to the specified endpoint
+                .when()                                             //Execute the POST request to the specified endpoint
                 .post(POSTS_ENDPOINT)
-                .then()                                             // THEN (Validation Step): Assertions on the server response
+                .then()                                             // Assertions on the server response
                 .statusCode(201).body("id", notNullValue())    // Data Validation (Hamcrest Matchers): Ensure the response contains a generated 'id'
-                .extract().as(PostModel.class);                     // Extraction: Deserialize the JSON response body back into a PostModel object
+                .extract().as(PostModel.class);                     // Deserialize the JSON response body back into a PostModel object
 
-        //Secondary Java Validation (Using JUnit Assertions):
-        //These checks are explicit and preferred for validating complex business logic.
         //Assert that the server successfully generated an ID
         assertNotNull(responseBody.getId(), "Generated ID should not be null in the response.");
 
